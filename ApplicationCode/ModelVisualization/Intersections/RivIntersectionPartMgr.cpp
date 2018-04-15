40f137a752d412b1f2a2e23ab8780bf056d1f435
bellout <mathias.bellout@ntnu.no>
2018-04-15 16:10:44 +0200

    Formatting.

--- ApplicationCode/ModelVisualization/Intersections/RivIntersectionPartMgr.cpp
+++ ApplicationCode/ModelVisualization/Intersections/RivIntersectionPartMgr.cpp
@@ -2,17 +2,17 @@
 //
 //  Copyright (C) Statoil ASA
 //  Copyright (C) Ceetron Solutions AS
-// 
+//
 //  ResInsight is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  ResInsight is distributed in the hope that it will be useful, but WITHOUT ANY
 //  WARRANTY; without even the implied warranty of MERCHANTABILITY or
 //  FITNESS FOR A PARTICULAR PURPOSE.
-// 
-//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
+//
+//  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
 //  for more details.
 //
 /////////////////////////////////////////////////////////////////////////////////
@@ -1102,7 +1102,9 @@ cvf::ref<RivIntersectionHexGridInterface> RivIntersectionPartMgr::createHexGridI
   if (eclipseView)
   {
     RigMainGrid* grid = eclipseView->mainGrid();
-    return new RivEclipseIntersectionGrid(grid, eclipseView->currentActiveCellInfo(), m_rimCrossSection->showInactiveCells());
+    return new RivEclipseIntersectionGrid(grid,
+                                          eclipseView->currentActiveCellInfo(),
+                                          m_rimCrossSection->showInactiveCells());
   }
 
   RimGeoMechView* geoView;
